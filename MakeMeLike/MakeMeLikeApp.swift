032c4328da0f461740ffba18c8040663bfdbd33d
horamupa <horamupa@gmail.com>
2022-11-17 22:31:45 +0400

    Initial Commit

---
+++ MakeMeLike/MakeMeLikeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MakeMeLikeApp.swift
+//  MakeMeLike
+//
+//  Created by MM on 17.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct MakeMeLikeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
